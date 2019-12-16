187a4463e8dc60a793839269935fa32dd036ca5a
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-16 17:44:17 +0100

    Toolbar icon badge removed after options page opened

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Welcome from './option-page/welcome.component.jsx';
 
+// rimuovo il badge
+chrome.browserAction.setBadgeText({text: ''});
+
 ReactDOM.render(
   <Welcome name="Ernesto" />,
   document.getElementById('root')
