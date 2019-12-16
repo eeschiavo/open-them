6ea764aecf37a561b27c15777de5fc17050e0c48
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-16 16:01:01 +0100

    Added loglevel configuration and prefix

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,46 @@
 'use strict';
+import * as log from 'loglevel';
+import * as prefix from 'loglevel-plugin-prefix';
+import React from 'react';
+import ReactDOM from 'reac-dom';
 
+prefix.reg(log);
+log.enableAll();
+
+prefix.apply(log, {
+  format(level, name, timestamp) {
+    return `(${timestamp}) ${level.toUpperCase()} ${name}:)}`;
+  },
+});
+
+prefix.apply(log.getLogger('critical'), {
+  format(level, name, timestamp) {
+    return `[${timestamp}] ${level} ${name}:`;
+  },
+});
+
+log.setLevel('trace');
+
+/*
+log.trace('trace');
+log.debug('debug');
+log.getLogger('critical').info('Something significant happened');
+log.log('log');
+log.info('info');
+log.warn('warn');
+log.error('error');
+*/
+
+/**
+* Apertura pagina options
+*/
 let openOptions = function() {
   console.log('Apro la pagina delle opzioni')
+  log.info('openOptions - apertura pagina options');
   chrome.runtime.openOptionsPage();
 }
+
+// rimuovo il badge
+chrome.browserAction.setBadgeText({text: ''});
 
 document.getElementById("open-options").addEventListener("click", openOptions);
