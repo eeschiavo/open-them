485fc83cf0728f3c02dee0c11d577bc42fb3db28
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-16 22:51:25 +0100

    It's now possible to save and remove links; they also opens automatically at extension startup

--- app/scripts.babel/option-page/welcome.component.jsx
+++ app/scripts.babel/option-page/welcome.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import * as log from 'loglevel';
+import AddLink from "./add-link.component.jsx";
 
 class Welcome extends React.Component {
 
@@ -9,32 +10,75 @@ class Welcome extends React.Component {
 
     log.info('Welcome - constructor');
 
-    // imposto dei link di prova
-    chrome.storage.sync.set(
-      {
-        urls: [
-          {
-            url: 'https://www.google.it',
-            incognito: false
-          },
-          {
-            url: 'https://www.facebook.com',
-            incognito: true
-          }]
-      },
-      function() {
+    this.state = {
+      links: []
+    };
 
-        log.debug('Welcome - url impostata');
+    chrome.storage.sync.get(['links'], (result) => {
+      log.debug(result);
+      this.setState({links: result.links});
+    });
+
+    this.modalCloseCallback = this.modalCloseCallback.bind(this);
+  }
+
+  /**
+   * Callback richiamata alla chiusura del modal del component AddLink
+   * @param value
+   */
+  modalCloseCallback(value) {
+    log.debug('Welcome - modalCloseCallback: ', value);
+    this.setState({
+      links: this.state.links.concat(value)
+    }, () => {
+
+      chrome.storage.sync.get(['links'], (result) => {
+
+        let array = result.links ? result.links : [];
+
+        let links = array.concat(this.state.links);
+        log.debug('Welcome - salvo i links: ', links);
+        chrome.storage.sync.set({links: links}, function() {
+          log.debug('Welcome - link aggiunto');
+        });
       });
+
+    });
+  }
+
+  /**
+   * Cancellazione di un link
+   */
+  removeLink(link, index) {
+
+    let links = this.state.links;
+    links.splice(index, 1);
+
+    log.debug('Welcome - links rimasti: ', links);
+
+    this.setState({
+      links: links
+    }, () => {
+      log.debug('Welcome - link rimosso dalla pagina');
+      chrome.storage.sync.set({links: this.state.links}, () => {
+        log.debug('Welcome - link rimosso dallo storage');
+
+      });
+    });
   }
 
   render() {
     return (
       <Container>
         <Row>
-          <Col>
-            Test
-          </Col>
+          {
+            this.state.links.map((link, index) => {
+              return (
+                <Col key={index} onClick={() => this.removeLink(link, index)}>{link.url}</Col>
+              )
+            })
+          }
+          <AddLink closeCallback={this.modalCloseCallback}/>
         </Row>
       </Container>
     )
