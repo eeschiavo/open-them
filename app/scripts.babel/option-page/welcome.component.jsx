87b17a837e76a8eba9610d276935903fb35badac
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-16 21:11:56 +0100

    Now links opens automatically at chrome start

--- app/scripts.babel/option-page/welcome.component.jsx
+++ app/scripts.babel/option-page/welcome.component.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import * as log from 'loglevel';
 
 class Welcome extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    log.info('Welcome - constructor');
+
+    // imposto dei link di prova
+    chrome.storage.sync.set(
+      {
+        urls: [
+          {
+            url: 'https://www.google.it',
+            incognito: false
+          },
+          {
+            url: 'https://www.facebook.com',
+            incognito: true
+          }]
+      },
+      function() {
+
+        log.debug('Welcome - url impostata');
+      });
+  }
+
   render() {
     return <h1>Welcome, {this.props.name}</h1>
   }
