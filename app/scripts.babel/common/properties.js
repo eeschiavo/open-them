c4c669851244c3b083567ef304c8fa797154c972
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-19 17:55:51 +0100

    Sidebar component branch

---
+++ app/scripts.babel/common/properties.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * URL per il recupero dell'icona da un URL
+ * @type {String}
+ */
+export const URL_BESTICON = 'https://otbesticon.herokuapp.com/icon?url=%domain%&size=80..120..200';
+
+/**
+ * Numero massimo di link salvabili
+ * @type {Number}
+ */
+export const MAX_VISIBLE_ITEMS = 30;
+
+/**
+ * Numero massimo di link in incognito salvabili
+ * @type {Number}
+ */
+export const MAX_INCOGNITO_ITEMS = 30;
