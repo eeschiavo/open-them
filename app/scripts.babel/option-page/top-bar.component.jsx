785e3f5c583eee14e65c9deec9af5929359a75d6
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-23 17:38:22 +0100

    Fixed links selection while edit mode is enabled

--- app/scripts.babel/option-page/top-bar.component.jsx
+++ app/scripts.babel/option-page/top-bar.component.jsx
@@ -8,9 +8,11 @@ class TopBar extends React.Component {
     super(props);
 
     this.state = {
-      editMode: false
-    }
+      editMode: false,
+      btnDisabled: true
+    };
 
+    this.changeButtonsStatus = this.changeButtonsStatus.bind(this);
     this.enableLinks = this.enableLinks.bind(this);
     this.disableLinks = this.disableLinks.bind(this);
     this.removeLinks = this.removeLinks.bind(this);
@@ -18,32 +20,55 @@ class TopBar extends React.Component {
     this.stopEdit = this.stopEdit.bind(this);
   }
 
+  /**
+   * Permette l'abilitazione multipla dei siti web selezionati
+   */
   enableLinks() {
     this.props.enableLinks();
   }
 
+  /**
+   * Permette la disabilitazione multipla dei siti we selezionati
+   */
   disableLinks() {
     this.props.disableLinks();
   }
 
+  /**
+   * Permette la cancellazione multipla dei siti we selezionati
+   */
   removeLinks() {
     this.props.removeLinks();
   }
 
+  /**
+   * Abilitazione e disabilizatione dei tasti della toolbar
+   * @param enable
+   */
+  changeButtonsStatus(enable) {
+    this.setState({btnDisabled: !enable});
+  }
+
+  /**
+   * Inizio della modalità di modifica
+   */
   startEdit() {
     log.info('TopBar - startEdit');
 
     if(!this.state.editMode) {
       this.props.enterEditMode(true);
-      this.setState({editMode: true});
+      this.setState({editMode: true, btnDisabled: true});
     }
   }
 
+  /**
+   * Fine della modalità di modifica
+   */
   stopEdit() {
     log.info('TopBar - stop');
 
-    this.props.enterEditMode(false);
-    this.setState({editMode: false});
+    this.props.enterEditMode(false, true);
+    this.setState({editMode: false, btnDisabled: true});
   }
 
   render() {
@@ -68,7 +93,8 @@ class TopBar extends React.Component {
 
                   <button
                     title={Localize('BUTTON_ENABLE_ALL_LINKS')}
-                    className="ot-button ot-button--cancel"
+                    className={'ot-button ot-button--cancel '+
+                      (this.state.btnDisabled ? 'ot-button--disabled':'')}
                     type="button"
                     onClick={this.enableLinks}>
                       <i className="fas fa-plus-square"></i>
@@ -76,7 +102,8 @@ class TopBar extends React.Component {
 
                   <button
                     title={Localize('BUTTON_DISABLE_ALL_LINKS')}
-                    className="ot-button ot-button--cancel"
+                    className={'ot-button ot-button--cancel '+
+                    (this.state.btnDisabled ? 'ot-button--disabled':'')}
                     type="button"
                     onClick={this.disableLinks}>
                       <i className="fas fa-minus-square"></i>
@@ -84,7 +111,8 @@ class TopBar extends React.Component {
 
                   <button
                     title={Localize('BUTTON_REMOVE_ALL_LINKS')}
-                    className="ot-button ot-button--cancel"
+                    className={'ot-button ot-button--cancel '+
+                    (this.state.btnDisabled ? 'ot-button--disabled':'')}
                     type="button"
                     onClick={this.removeLinks}>
                       <i className="fas fa-trash"></i>
