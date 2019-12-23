98426609ec61a9d6d0845c42a03f86f55082d326
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-23 13:05:26 +0100

    Modal window fix

--- app/scripts.babel/option-page/options-page.component.jsx
+++ app/scripts.babel/option-page/options-page.component.jsx
@@ -22,11 +22,15 @@ class OptionsPage extends React.Component {
       page: 1
     }
 
+    this.linksPageRef = React.createRef();
+
     this.changePage = this.changePage.bind(this);
   }
 
   changePage(page) {
-    this.setState({page})
+    this.setState({page}, () => {
+      this.linksPageRef.current.closeModals();
+    });
   }
 
   render() {
@@ -34,7 +38,7 @@ class OptionsPage extends React.Component {
       <div className="options-page">
         <Sidebar changePage={this.changePage} />
         <div className={(this.state.page == 1 ? 'options-page--show':'options-page--hide')}>
-          <LinksPage />
+          <LinksPage ref={this.linksPageRef} />
         </div>
         <div className={(this.state.page == 2 ? 'options-page--show':'options-page--hide')}>
           <SettingsPage />
