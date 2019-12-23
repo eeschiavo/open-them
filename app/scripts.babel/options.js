14194b86fcf7ba1a5000fb273ab304d71e17cb4e
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2019-12-23 17:31:01 +0100

    Footer created

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,10 +1,10 @@
 'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import OptionsPage from './option-page/options-page.component.jsx';
+import Bootstrap from './option-page/bootstrap.component.jsx';
 import * as log from 'loglevel';
 
 ReactDOM.render(
-  <OptionsPage />,
+  <Bootstrap />,
   document.getElementById('options-root')
 );
